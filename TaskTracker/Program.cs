79e75dc0683f2b7afdbeb5e68a85158fa8d19b77
colnkor <mr.colnkor@gmail.com>
2026-01-03 11:50:12 +0300

    Добавьте файлы проекта.

---
+++ TaskTracker/Program.cs
@@ -0,0 +1,161 @@
+﻿
+using System.Text.Json;
+
+namespace Program
+{
+    class Program
+    {
+        static public void CreateTask(string name, string description)
+        {
+            TaskTracker.Task aTask = new(name, description);
+            aTask.ToJson();
+            Console.WriteLine($"New task file created at: {TaskTracker.Task.TASKSPATH}");
+            Console.WriteLine($"Task id: {aTask.id}");
+        }
+
+        static public TaskTracker.Task FindTask(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                throw new Exception("ID cannot be empty!");
+            }
+
+            return TaskTracker.Task.FromJson(Path.Combine(TaskTracker.Task.TASKSPATH, id + ".json"));
+        }
+        
+        static public void RemoveTask(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                throw new Exception("ID cannot be empty!");
+            }
+
+            File.Delete(Path.Combine(TaskTracker.Task.TASKSPATH, id + ".json"));
+            Console.WriteLine("Removed.");
+        }
+
+        static public void UpdateTask(string id, string description)
+        {
+            if (String.IsNullOrWhiteSpace(description))
+            {
+                throw new Exception("Description cannot be empty!");
+            }
+
+            var task = FindTask(id);
+            task.Description = description;
+            task.ToJson();
+            Console.WriteLine($"Task (id: {task.id}) has been updated.\n{task}");
+        }
+
+        static public void MarkTask(string id, TaskTracker.TaskStatus newStatus) 
+        {
+            if (!Enum.IsDefined(typeof(TaskTracker.TaskStatus), (int)newStatus))
+            {
+                throw new Exception("New value of status should be valid!");
+            }
+            var task = FindTask(id);
+            task.Status = newStatus;
+            task.ToJson();
+        }
+
+        static public void ShowTask(string id)
+        {
+            var aTask = FindTask(id);
+            Console.WriteLine(aTask);
+        }
+
+        static public void ListTasks(string? option = null)
+        {
+            var status_filter = option switch
+            {
+                "done" => (TaskTracker.TaskStatus.DONE),
+                "todo" => (TaskTracker.TaskStatus.TODO),
+                "in-progress" => (TaskTracker.TaskStatus.INPROGRESS),
+                _ => (TaskTracker.TaskStatus)(-1)
+            };
+
+            string[] files = Directory.GetFiles(TaskTracker.Task.TASKSPATH);
+
+            Console.WriteLine("id, name, status, created_at, updated_at, description");
+            foreach (string file in files)
+            {
+                var task = TaskTracker.Task.FromJson(file);
+                if ((int)status_filter == -1 || status_filter == task.Status)
+                {
+                    Console.WriteLine(task);
+                }
+            }
+            Console.WriteLine("+++++++++++++++++++++++++");
+        }
+
+        static public void Main(string[] args)
+        {
+            string command = args.Length > 0 ? args[0] : "help";
+
+            try
+            {
+                switch (command)
+                {
+                    case "add" when args.Length == 3:
+                        CreateTask(args[1], args[2]);
+                        break;
+                    case "update" when args.Length == 3:
+                        UpdateTask(args[1], args[2]);
+                        break;
+                    case "remove" when args.Length == 2:
+                    case "delete" when args.Length == 2:
+                        RemoveTask(args[1]);
+                        break;
+                    case "show" when args.Length == 2:
+                        ShowTask(args[1]);
+                        break;
+                    case "mark-todo" when args.Length == 2:
+                        MarkTask(args[1], TaskTracker.TaskStatus.TODO);
+                        break;
+                    case "mark-in-progress" when args.Length == 2:
+                        MarkTask(args[1], TaskTracker.TaskStatus.INPROGRESS);
+                        break;
+                    case "mark-done" when args.Length == 2:
+                        MarkTask(args[1], TaskTracker.TaskStatus.DONE);
+                        break;
+                    case "get-curr-folder":
+                        Console.WriteLine(TaskTracker.Task.TASKSPATH);
+                        break;
+                    case "list":
+                        ListTasks((args.Length > 1) ? args[1] : null);
+                        break;
+                    case "help":
+                    default:
+                        Console.WriteLine("""
+                            AVAILABLE COMMANDS
+                                add [name] [description]                - create new task with name & description
+                                update [id] [description]               - update task by id
+                                delete [id]                             - delete task by id
+                                show [id]                               - show task by id
+                                mark-todo [id]                          - mark todo
+                                mark-in-progress [id]                   - mark in progress by id
+                                mark-done [id]                          - mark done by id
+                                list [?type: done | todo | in-progress] - list all tasks
+                                get-curr-folder                         - current working directory
+                            """);
+                        break;
+                }
+            }
+            catch (JsonException jex)
+            {
+                Console.WriteLine(jex.Message);
+                Console.WriteLine("It seems, that json file of task is corrupted! Oh uh...");
+            }
+            catch (InvalidOperationException ipex)
+            {
+                Console.WriteLine(ipex.Message);
+                Console.WriteLine("Something went wrong while parsing task properties");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Write help command to show all commands of CLI tool");
+            }
+        }
+    }
+}
---
+++ TaskTracker/Task.cs
@@ -0,0 +1,140 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace TaskTracker
+{
+    internal enum TaskStatus { TODO, INPROGRESS, DONE }
+    internal class Task
+    {
+
+        private static Dictionary<int, string> StatusMap = new() {
+            { 0, "to-do" },
+            { 1, "in-progress" },
+            { 2, "done" },
+        };
+        public static readonly string TASKSPATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "taskscli");
+
+        public readonly string id;
+        private string _name;
+        private string _description;
+        private TaskStatus _status = TaskStatus.TODO;
+        private string _updated_at = DateTime.Now.ToString();
+        private string _created_at = DateTime.Now.ToString();
+
+        public string Description { 
+            get { return _description; } 
+            set { 
+                _description = value;
+                _updated_at = DateTime.Now.ToString();
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                _updated_at = DateTime.Now.ToString();
+            }
+        }
+
+        public TaskStatus Status
+        {
+            get { return _status; }
+            set
+            {
+                if (!Enum.IsDefined(typeof(TaskStatus), (int)value))
+                {
+                    throw new Exception("New value of status should be valid!");
+                }
+                _status = value;
+                _updated_at = DateTime.Now.ToString();
+            }
+        }
+
+        override public string ToString()
+        {
+            return $"{id}, {Name}, {StatusMap[(int)Status]}, {_created_at}, {_updated_at}, {Description}";
+        }
+
+        public Task(string name, string description) {
+            _name = (String.IsNullOrWhiteSpace(name)) ? "no name" : name.Trim();
+            _description = (String.IsNullOrWhiteSpace(description)) ? "no desc" : description.Trim();
+
+            string SourceData = _name + _description + _created_at;
+            byte[] tmpSource = UTF8Encoding.UTF8.GetBytes(SourceData);
+            byte[] tmpHash = MD5.Create().ComputeHash(tmpSource);
+
+            StringBuilder sb = new();
+            foreach (byte b in tmpHash) {
+                sb.Append(b.ToString("x2"));
+            }
+
+            id = sb.ToString();
+        }
+
+        private Task(string id) { this.id = id; }
+
+        public static Task FromJson(string path)
+        {
+            using var stream = File.OpenRead(path);
+            using var document = JsonDocument.Parse(stream);
+            
+            JsonElement root = document.RootElement;
+            string id = root.GetProperty("Id").GetString() ?? "";
+            string name = root.GetProperty("Name").GetString() ?? "";
+            int status = root.GetProperty("Status").GetInt32();
+            string description = root.GetProperty("Description").GetString() ?? "";
+            string created_at = root.GetProperty("Created_at").GetString() ?? "";
+            string updated_at = root.GetProperty("Updated_at").GetString() ?? "";
+
+            string[] fields = { id, name, description, created_at, updated_at };
+
+            if (fields.Any(string.IsNullOrWhiteSpace))
+            {
+                throw new InvalidOperationException("One or more required fields are missing or empty. Broken JSON.");
+            }
+            if (!DateTime.TryParse(created_at, out _) || !DateTime.TryParse(updated_at, out _))
+            {
+                throw new InvalidOperationException("Creating or Update times are incorrect. Broken JSON.");
+            }
+            if (!Enum.IsDefined(typeof(TaskStatus), status))
+            {
+                throw new InvalidOperationException("Status propery is incorrect for Task. Broken JSON.");
+            }
+
+            return new Task(id) { 
+                _name = name,
+                _description = description,
+                _status = (TaskStatus)status,
+                _created_at = created_at,
+                _updated_at = updated_at,
+            };
+        }
+
+        public void ToJson()
+        {
+            var options = new JsonWriterOptions { Indented = true };
+            if (!Directory.Exists(TASKSPATH))
+            {
+                Directory.CreateDirectory(TASKSPATH);
+            }
+
+            using (var stream = File.Create(Path.Combine(TASKSPATH, id + ".json")))
+            using (var writer = new Utf8JsonWriter(stream, options))
+            {
+                writer.WriteStartObject();
+                writer.WriteString("Id", id);
+                writer.WriteString("Name", Name);
+                writer.WritePropertyName("Status");
+                writer.WriteNumberValue(((int)_status));
+                writer.WriteString("Description", Description);
+                writer.WriteString("Created_at", _created_at);
+                writer.WriteString("Updated_at", _updated_at);
+                writer.WriteEndObject();
+            }
+        }
+    }
+}
